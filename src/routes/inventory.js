97061eb54a1f9a49ff8977b96b8a6aa14cae012e
danliris <danliris.cloud@gmail.com>
2018-02-08 11:12:17 +0700

    Update Routes Inventory
    Disabled Routes Inventory

--- src/routes/inventory.js
+++ src/routes/inventory.js
@@ -3,7 +3,7 @@ module.exports = [
         route: 'inventory/packing-receipt',
         name: 'packing-receipt',
         moduleId: './modules/inventory/packing-receipt/index',
-        nav: true,
+        nav: false,
         title: 'Penerimaan Packing Gudang Jadi',
         auth: true,
         settings: {
@@ -16,7 +16,7 @@ module.exports = [
         route: 'inventory/inventory-documents',
         name: 'inventory/inventory-documents',
         moduleId: './modules/inventory/inventory-document/index',
-        nav: true,
+        nav: false,
         title: 'Dokumen Inventory',
         auth: true,
         settings: {
@@ -29,7 +29,7 @@ module.exports = [
         route: 'inventory/reports/inventory-movement-report',
         name: 'inventory/reports/inventory-movement-report',
         moduleId: './modules/inventory/reports/inventory-movement-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Stock In/Out',
         auth: true,
         settings: {
@@ -42,7 +42,7 @@ module.exports = [
         route: 'inventory/reports/inventory-summary-report',
         name: 'inventory/reports/inventory-summary-report',
         moduleId: './modules/inventory/reports/inventory-summary-report/index',
-        nav: true,
+        nav: false,
         title: 'Kartu Stok',
         auth: true,
         settings: {
@@ -55,7 +55,7 @@ module.exports = [
         route: 'inventory/reports/fp-packing-receipt-report',
         name: 'inventory/reports/fp-packing-receipt-report',
         moduleId: './modules/inventory/reports/fp-packing-receipt-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Penerimaan Packing',
         auth: true,
         settings: {
@@ -68,7 +68,7 @@ module.exports = [
         route: 'inventory/finishing-printing/fp-retur-to-qc-doc',
         name: 'inventory/finishing-printing/fp-retur-to-qc-doc',
         moduleId: './modules/inventory/finishing-printing/fp-retur-to-qc-doc/index',
-        nav: true,
+        nav: false,
         title: 'Retur Barang ke QC',
         auth: true,
         settings: {
@@ -82,7 +82,7 @@ module.exports = [
         route: "inventory/finishing-printing/reports/fp-retur-to-qc-doc-report",
         name: "inventory/finishing-printing/reports/fp-retur-to-qc-doc-report",
         moduleId: "modules/inventory/finishing-printing/reports/fp-retur-to-qc-doc-report/index",
-        nav: true,
+        nav: false,
         title: "Laporan Retur Barang ke QC",
         auth: true,
         settings: {
@@ -95,7 +95,7 @@ module.exports = [
         route: "inventory/finishing-printing/fp-shipment-document",
         name: "inventory/finishing-printing/fp-shipment-document",
         moduleId: "modules/inventory/finishing-printing/fp-shipment-document/index",
-        nav: true,
+        nav: false,
         title: "Pengiriman Barang Gudang Jadi",
         auth: true,
         settings: {
@@ -108,7 +108,7 @@ module.exports = [
         route: "inventory/finishing-printing/reports/fp-shipment-document-report",
         name: "inventory/finishing-printing/reports/fp-shipment-document-report",
         moduleId: "modules/inventory/finishing-printing/reports/fp-shipment-document-report/index",
-        nav: true,
+        nav: false,
         title: "Laporan Pengiriman Barang Gudang Jadi",
         auth: true,
         settings: {
@@ -121,7 +121,7 @@ module.exports = [
         route: "inventory/finishing-printing/fp-retur-from-buyer",
         name: "inventory/finishing-printing/fp-retur-from-buyer",
         moduleId: "modules/inventory/finishing-printing/fp-retur-from-buyer/index",
-        nav: true,
+        nav: false,
         title: "Retur Barang Dari Buyer",
         auth: true,
         settings: {
@@ -134,7 +134,7 @@ module.exports = [
         route: "inventory/reports/fp-shiptment-delivery-buyer-report",
         name: "inventory/reports/fp-shiptment-delivery-buyer-report",
         moduleId: "modules/inventory/reports/fp-shiptment-delivery-buyer-report/index",
-        nav: true,
+        nav: false,
         title: "Laporan Pengiriman Buyer",
         auth: true,
         settings: {
