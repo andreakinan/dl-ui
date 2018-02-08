eb6cf429c5ff79a10dcec01a72255af66f556c39
danliris <danliris.cloud@gmail.com>
2018-02-08 11:26:03 +0700

    Update report.js

--- src/routes/report.js
+++ src/routes/report.js
@@ -1,14 +1,14 @@
 module.exports = [
     {
-        route: 'power-bi/purchasing/textile',
-        name: 'power-bi-purchasing-textile',
-        moduleId: './modules/power-bi/purchasing-textile/index',
+        route: 'power-bi/purchasing',
+        name: 'power-bi-purchasing',
+        moduleId: './modules/power-bi/purchasing/index',
         nav: true,
-        title: 'Power BI: Textile Purchasing Reports',
+        title: 'Power BI: Purchasing Reports',
         auth: true,
         settings: {
             group: "reports",
-            permission : {"P1": 7, "P3": 7, "P4": 7, "P6": 7, "P7": 7, "C9": 1, "PG": 7},
+            permission : {"U11": 1, "U17": 1},
             iconClass: 'fa fa-dashboard'
         }
     },
@@ -16,7 +16,7 @@ module.exports = [
         route: 'power-bi/purchasing/garment',
         name: 'power-bi-purchasing-garment',
         moduleId: './modules/power-bi/purchasing-garment/index',
-        nav: true,
+        nav: false,
         title: 'Power BI: Garment Purchasing Reports',
         auth: true,
         settings: {
@@ -29,7 +29,7 @@ module.exports = [
         route: 'power-bi/finishing-printing',
         name: 'power-bi-finishing-printing',
         moduleId: './modules/power-bi/finishing-printing/index',
-        nav: true,
+        nav: false,
         title: 'Power BI: Finishing Printing Reports',
         auth: true,
         settings: {
@@ -41,7 +41,7 @@ module.exports = [
         route: 'power-bi/sales',
         name: 'power-bi-sales',
         moduleId: './modules/power-bi/sales/index',
-        nav: true,
+        nav: false,
         title: 'Power BI: Sales Reports',
         auth: true,
         settings: {
@@ -53,7 +53,7 @@ module.exports = [
         route: 'power-bi/inventory',
         name: 'power-bi-inventory',
         moduleId: './modules/power-bi/inventory/index',
-        nav: true,
+        nav: false,
         title: 'Power BI: Inventory Reports',
         auth: true,
         settings: {
@@ -71,7 +71,7 @@ module.exports = [
         auth: true,
         settings: {
             group: "reports",
-            permission: { "A2": 1, "C9": 1, "PGA":1, "PA":1, "PM":1, "PE":1 },
+            permission: { "U11": 1, "U14": 1 },
             iconClass: 'fa fa-dashboard'
         }
     }]
