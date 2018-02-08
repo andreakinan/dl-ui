ef655514613cdc46891e6291a07440a6bd18a48b
danliris <danliris.cloud@gmail.com>
2018-02-08 11:18:05 +0700

    Update production.js

--- src/routes/production.js
+++ src/routes/production.js
@@ -3,7 +3,7 @@ module.exports = [
         route: 'production/spinning/winding/winding-quality-sampling',
         name: 'winding-quality-sampling',
         moduleId: './modules/production/spinning/winding/winding-quality-sampling/index',
-        nav: true,
+        nav: false,
         title: 'Quality Hasil Produksi Spinning',
         auth: true,
         settings: {
@@ -17,7 +17,7 @@ module.exports = [
         route: 'production/spinning/winding/winding-quality-sampling/report',
         name: 'winding-quality-sampling-report',
         moduleId: './modules/production/spinning/winding/reports/winding-quality-sampling-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Quality Hasil Produksi Spinning',
         auth: true,
         settings: {
@@ -30,7 +30,7 @@ module.exports = [
         route: 'production/spinning/winding/winding-production-output',
         name: 'winding-production-output',
         moduleId: './modules/production/spinning/winding/winding-production-output/index',
-        nav: true,
+        nav: false,
         title: 'Output Hasil Produksi Spinning',
         auth: true,
         settings: {
@@ -43,7 +43,7 @@ module.exports = [
         route: 'production/spinning/winding/reports/daily-spinning-production-report',
         name: 'daily-spinning-production-report',
         moduleId: './modules/production/spinning/winding/reports/daily-spinning-production-report/index',
-        nav: true,
+        nav: false,
         title: 'Monitoring Output Hasil Produksi Spinning',
         auth: true,
         settings: {
@@ -56,7 +56,7 @@ module.exports = [
         route: 'production/finishing-printing/daily-operation',
         name: 'dailys',
         moduleId: './modules/production/finishing-printing/daily-operation/index',
-        nav: true,
+        nav: false,
         title: 'Monitoring Operasional Harian',
         auth: true,
         settings: {
@@ -69,7 +69,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/daily-operation-report',
         name: 'report-dailys',
         moduleId: './modules/production/finishing-printing/reports/daily-operation-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Monitoring Operasional Harian',
         auth: true,
         settings: {
@@ -82,7 +82,7 @@ module.exports = [
         route: 'production/finishing-printing/monitoring-event',
         name: 'monitoring-event',
         moduleId: './modules/production/finishing-printing/monitoring-event/index',
-        nav: true,
+        nav: false,
         title: 'Monitoring Event',
         auth: true,
         settings: {
@@ -95,7 +95,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/monitoring-event-report',
         name: 'report-monitoring-events',
         moduleId: './modules/production/finishing-printing/reports/monitoring-event-report/index',
-        nav: true,
+        nav: false,
         title: 'Report Monitoring Event',
         auth: true,
         settings: {
@@ -109,7 +109,7 @@ module.exports = [
         route: 'production/finishing-printing/monitoring-specification-machine',
         name: 'monitoring-specification-machine',
         moduleId: './modules/production/finishing-printing/monitoring-specification-machine/index',
-        nav: true,
+        nav: false,
         title: 'Monitoring Spesifikasi Mesin',
         auth: true,
         settings: {
@@ -122,7 +122,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/monitoring-specification-machine-report',
         name: 'monitoring-specification-machine-report',
         moduleId: './modules/production/finishing-printing/reports/monitoring-specification-machine-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Monitoring Spesifikasi Mesin',
         auth: true,
         settings: {
@@ -135,7 +135,7 @@ module.exports = [
         route: 'production/finishing-printing/kanban',
         name: 'kanban',
         moduleId: './modules/production/finishing-printing/kanban/index',
-        nav: true,
+        nav: false,
         title: 'Kanban',
         auth: true,
         settings: {
@@ -148,7 +148,7 @@ module.exports = [
         route: 'production/finishing-printing/bad-output-kanban',
         name: 'kanban',
         moduleId: './modules/production/finishing-printing/bad-output-kanban/index',
-        nav: true,
+        nav: false,
         title: 'Kanban Pengganti Bad Output',
         auth: true,
         settings: {
@@ -161,7 +161,7 @@ module.exports = [
         route: 'production/finishing-printing/kanban-visualization-area',
         name: 'kanban-visualization-area',
         moduleId: './modules/production/finishing-printing/kanban-visualization-area/index',
-        nav: true,
+        nav: false,
         title: 'Kanban Visualization Area',
         auth: true,
         settings: {
@@ -174,7 +174,7 @@ module.exports = [
         route: 'production/finishing-printing/kanban-visualization-machine',
         name: 'kanban-visualization-machine',
         moduleId: './modules/production/finishing-printing/kanban-visualization-machine/index',
-        nav: true,
+        nav: false,
         title: 'Kanban Visualization Machine',
         auth: true,
         settings: {
@@ -187,7 +187,7 @@ module.exports = [
         route: 'production/finishing-printing/monitoring-kanban',
         name: 'monitoring-kanban',
         moduleId: './modules/production/finishing-printing/monitoring-kanban/index',
-        nav: true,
+        nav: false,
         title: 'Monitoring Kanban',
         auth: true,
         settings: {
@@ -200,7 +200,7 @@ module.exports = [
         route: 'production/finishing-printing/quality-control/defect',
         name: 'report-dailys',
         moduleId: './modules/production/finishing-printing/quality-control/defect/index',
-        nav: true,
+        nav: false,
         title: 'Pencatatan Pemeriksaan Kain',
         auth: true,
         settings: {
@@ -213,7 +213,7 @@ module.exports = [
         route: 'finishing-printing/reports/fabric-quality-control-report',
         name: 'fabric-quality-control-report',
         moduleId: './modules/production/finishing-printing/reports/fabric-quality-control-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Pemeriksaan Kain',
         auth: true,
         settings: {
@@ -226,7 +226,7 @@ module.exports = [
         route: 'production/finishing-printing/packing',
         name: 'packing',
         moduleId: './modules/production/finishing-printing/packing/index',
-        nav: true,
+        nav: false,
         title: 'Penyerahan Produksi',
         auth: true,
         settings: {
@@ -239,7 +239,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/packing-report',
         name: 'packing-report',
         moduleId: './modules/production/finishing-printing/reports/packing-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Penyerahan Produksi',
         auth: true,
         settings: {
@@ -252,7 +252,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/qcgudang-report',
         name: 'packing-report',
         moduleId: './modules/production/finishing-printing/reports/qcgudang-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Penyerahan QC ke Gudang',
         auth: true,
         settings: {
@@ -265,7 +265,7 @@ module.exports = [
         route: 'production/finishing-printing/inspection-lot-color',
         name: 'inspection-lot-colors',
         moduleId: './modules/production/finishing-printing/inspection-lot-color/index',
-        nav: true,
+        nav: false,
         title: 'Pencatatan Pemeriksaan Lot Warna',
         auth: true,
         settings: {
@@ -278,7 +278,7 @@ module.exports = [
         route: 'finishing-printing/reports/inspection-lot-color-report',
         name: 'inspection-lot-color-report',
         moduleId: './modules/production/finishing-printing/reports/inspection-lot-color-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Pemeriksaan Lot Warna',
         auth: true,
         settings: {
@@ -291,7 +291,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/bad-output-report',
         name: 'badoutput',
         moduleId: './modules/production/finishing-printing/reports/bad-output-report/index',
-        nav: true,
+        nav: false,
         title: 'Monitoring Good / Bad Output per Mesin',
         auth: true,
         settings: {
@@ -304,7 +304,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/monitoring-montly-operation-machine',
         name: 'monitoring-montly-operation-machine',
         moduleId: './modules/production/finishing-printing/reports/monitoring-montly-operation-machine/index',
-        nav: true,
+        nav: false,
         title: 'Monitoring monthly operation machine',
         auth: true,
         settings: {
@@ -317,7 +317,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/daily-operation-machine',
         name: 'daily-operation-machine',
         moduleId: './modules/production/finishing-printing/reports/daily-operation-machine/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Output Mesin per Hari',
         auth: true,
         settings: {
@@ -330,7 +330,7 @@ module.exports = [
         route: 'production/finishing-printing/reports/machine-queue-report',
         name: 'machine-queue-report',
         moduleId: './modules/production/finishing-printing/reports/machine-queue-report/index',
-        nav: true,
+        nav: false,
         title: 'Laporan Order Belum Diproduksi Mesin',
         auth: true,
         settings: {
